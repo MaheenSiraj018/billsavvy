2b4f22587691d716b0257d1434fdb68652b0d507
AlishbaMumtaz <MUMTAZ4403858@cloud.neduet.edu.pk>
2024-01-11 17:11:33 +0500

    commit

---
+++ blog-app/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './App.css';
+import Blog from './pages/Blog';
+import Home from './pages/Home';
+import {Route,Routes,BrowserRouter} from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="container">
+     
+      <Routes>
+      <Route path="/"  element={<Home />} />
+      <Route path="/blog/:id"  element={<Blog/>} />
+      </Routes>
+      
+    </div>
+  );
+}
+
+export default App;
+
+
